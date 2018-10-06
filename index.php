02f3fa8c23538fa56a5f74a3dd880a03349ae270
zohar pfeffer <pfzohar@gmail.com>
2018-10-06 10:40:27 +0300

    added scroll reveal

--- index.php
+++ index.php
@@ -7,17 +7,17 @@ include_once "header.php";
         <section id="benefits">
             <div class="container">
                 <div class="row justify-content-center">
-                    <div id="happy-costumers" class="col-12 col-md-6 col-xl-4 text-center mt-md-3">
+                    <div id="happy-costumers" class="col-12 col-md-6 col-xl-4 text-center mt-md-3 benefits-reveal">
                         <i class="far fa-grin"></i>
                         <h3>לקוחות מרוצים</h3>
                         <p>טקסט למילוי מקום טקסט למילוי מקום טקסט למילוי מקום טקסט למילוי מקום טקסט למילוי מקום טקסט למילוי מקום טקסט למילוי מקום</p>
                     </div>
-                    <div id="personal-service" class="col-12 col-md-6 col-xl-4 text-center mt-3">
+                    <div id="personal-service" class="col-12 col-md-6 col-xl-4 text-center mt-3 benefits-reveal">
                         <i class="far fa-handshake"></i>
                         <h3>שירות אישי</h3>
                         <p>טקסט למילוי מקום טקסט למילוי מקום טקסט למילוי מקום טקסט למילוי מקום טקסט למילוי מקום טקסט למילוי מקום טקסט למילוי מקום</p>
                     </div>
-                    <div id="professional" class="col-12 col-md-6 col-xl-4 text-center mt-3 mt-md-5 mt-xl-3">
+                    <div id="professional" class="col-12 col-md-6 col-xl-4 text-center mt-3 mt-md-5 mt-xl-3 benefits-reveal">
                         <i class="fas fa-award"></i>
                         <h3>מקצועיות</h3>
                         <p>טקסט למילוי מקום טקסט למילוי מקום טקסט למילוי מקום טקסט למילוי מקום טקסט למילוי מקום טקסט למילוי מקום טקסט למילוי מקום</p>
@@ -92,22 +92,22 @@ include_once "header.php";
         <section id="services">
             <div class="container-fluid pt-5 pt-sm-0">
                 <div class="row">
-                    <a id="home-create" class="col-12 col-md-6 col-xl-4 mt-4" href="#">
+                    <a id="home-create" class="col-12 col-md-6 col-xl-4 mt-4 service-item services-reveal" href="#">
                         <h3 class="text-center">הקמה ופיתוח</h3>
                     </a>
-                    <a id="home-maintain" class="col-12 col-md-6 col-xl-4 mt-4" href="#">
+                    <a id="home-maintain" class="col-12 col-md-6 col-xl-4 mt-4 service-item services-reveal" href="#">
                         <h3 class="text-center">אחזקה שוטפת</h3>
                     </a>
-                    <a id="home-fix" class="col-12 col-md-6 col-xl-4 mt-4" href="#">
+                    <a id="home-fix" class="col-12 col-md-6 col-xl-4 mt-4 service-item services-reveal" href="#">
                         <h3 class="text-center">שיקום</h3>
                     </a>
-                    <a id="home-irrigation" class="col-12 col-md-6 col-xl-4 mt-4" href="#">
+                    <a id="home-irrigation" class="col-12 col-md-6 col-xl-4 mt-4 service-item services-reveal" href="#">
                         <h3 class="text-center">מערכות השקייה</h3>
                     </a>
-                    <a id="home-walkways" class="col-12 col-md-6 col-xl-4 mt-4" href="#">
+                    <a id="home-walkways" class="col-12 col-md-6 col-xl-4 mt-4 service-item services-reveal" href="#">
                         <h3 class="text-center">שבילים</h3>
                     </a>
-                    <a id="home-ground" class="col-12 col-md-6 col-xl-4 mt-4" href="#">
+                    <a id="home-ground" class="col-12 col-md-6 col-xl-4 mt-4 service-item services-reveal" href="#">
                         <h3 class="text-center">חיפוי קרקע</h3>
                     </a>
                 </div>
@@ -119,7 +119,7 @@ include_once "header.php";
                 <!-- gallery START -->
                 <section id="gallery" class="col-xl-8 mb-3">
                     <div class="product-slider">
-                        <div id="carousel" class="carousel slide" data-ride="carousel">
+                        <div id="carousel" class="carousel slide reveal" data-ride="carousel">
                             <div class="carousel-inner">
                                 <div class="carousel-item active">
                                     <img class="d-block w-100" src="img/bench-house-bg.jpg" alt="ספסל על רקע בית">
@@ -171,18 +171,18 @@ include_once "header.php";
                         <div id="thumbcarousel" class="carousel slide d-none d-md-block" data-interval="false" dir="ltr">
                             <div class="carousel-inner">
                                 <div class="item active">
-                                    <div data-target="#carousel" data-slide-to="0" class="thumb"><img src="img/bench-house-bg.jpg"></div
-                                    ><div data-target="#carousel" data-slide-to="1" class="thumb"><img src="img/bench-house-bg.jpg"></div
-                                    ><div data-target="#carousel" data-slide-to="2" class="thumb"><img src="img/bench-house-bg.jpg"></div
-                                    ><div data-target="#carousel" data-slide-to="3" class="thumb"><img src="img/bench-house-bg.jpg"></div
-                                    ><div data-target="#carousel" data-slide-to="4" class="thumb"><img src="img/bench-house-bg.jpg"></div
-                                    ><div data-target="#carousel" data-slide-to="5" class="thumb"><img src="img/bench-house-bg.jpg"></div
-                                    ><div data-target="#carousel" data-slide-to="6" class="thumb"><img src="img/bench-house-bg.jpg"></div
-                                    ><div data-target="#carousel" data-slide-to="7" class="thumb"><img src="img/bench-house-bg.jpg"></div
-                                    ><div data-target="#carousel" data-slide-to="8" class="thumb"><img src="img/bench-house-bg.jpg"></div
-                                    ><div data-target="#carousel" data-slide-to="9" class="thumb"><img src="img/bench-house-bg.jpg"></div
-                                    ><div data-target="#carousel" data-slide-to="10" class="thumb"><img src="img/bench-house-bg.jpg"></div
-                                    ><div data-target="#carousel" data-slide-to="11" class="thumb"><img src="img/bench-house-bg.jpg"></div>
+                                    <div data-target="#carousel" data-slide-to="0" class="thumb thumb-reveal"><img src="img/bench-house-bg.jpg"></div
+                                    ><div data-target="#carousel" data-slide-to="1" class="thumb thumb-reveal"><img src="img/bench-house-bg.jpg"></div
+                                    ><div data-target="#carousel" data-slide-to="2" class="thumb thumb-reveal"><img src="img/bench-house-bg.jpg"></div
+                                    ><div data-target="#carousel" data-slide-to="3" class="thumb thumb-reveal"><img src="img/bench-house-bg.jpg"></div
+                                    ><div data-target="#carousel" data-slide-to="4" class="thumb thumb-reveal"><img src="img/bench-house-bg.jpg"></div
+                                    ><div data-target="#carousel" data-slide-to="5" class="thumb thumb-reveal"><img src="img/bench-house-bg.jpg"></div
+                                    ><div data-target="#carousel" data-slide-to="6" class="thumb thumb-reveal"><img src="img/bench-house-bg.jpg"></div
+                                    ><div data-target="#carousel" data-slide-to="7" class="thumb thumb-reveal"><img src="img/bench-house-bg.jpg"></div
+                                    ><div data-target="#carousel" data-slide-to="8" class="thumb thumb-reveal"><img src="img/bench-house-bg.jpg"></div
+                                    ><div data-target="#carousel" data-slide-to="9" class="thumb thumb-reveal"><img src="img/bench-house-bg.jpg"></div
+                                    ><div data-target="#carousel" data-slide-to="10" class="thumb thumb-reveal"><img src="img/bench-house-bg.jpg"></div
+                                    ><div data-target="#carousel" data-slide-to="11" class="thumb thumb-reveal"><img src="img/bench-house-bg.jpg"></div>
                                 </div>
                             </div>
                         </div>
@@ -192,26 +192,32 @@ include_once "header.php";
                     <!-- gallery END -->
                     <!-- testimonials START -->
                     <section id="testimonials" class="col-xl-4 px-3">
-                        <blockquote id="testimmonial-1" class="blockquote">
-                            <i class="fas fa-quote-right"></i>
-                            <p class="text-center">
-                                צציטוט ההמלצה של הלקוח והחוויה החיובית והנפלאה שלו עם תום, לנסות להוציא מהלקוחות המלצה של שניים שלושה משפטים<br><i class="fas fa-quote-left d-block text-left"></i></p>
-                        </blockquote>
-                        <cite class="d-block text-left">שם הלקוח</cite>
-                        <hr class="w-50">
-                        <blockquote id="testimmonial-2" class="blockquote">
-                            <i class="fas fa-quote-right"></i>
-                            <p class="text-center">
-                                יטוט ההמלצה של הלקוח והחוויה החיובית והנפלאה שלו עם תום, לנסות להוציא מהלקוחות המלצה של שניים שלושה משפטים<br><i class="fas fa-quote-left d-block text-left"></i></p>
-                        </blockquote>
-                        <cite class="d-block text-left">שם הלקוח</cite>
-                        <hr class="w-50">
-                        <blockquote id="testimmonial-3" class="blockquote">
-                            <i class="fas fa-quote-right"></i>
-                            <p class="text-center">
-                                צציטוט ההמלצה של הלקוח והחוויה החיובית והנפלאה שלו עם תום, לנסות להוציא מהלקוחות המלצה של שניים שלושה משפטים<br><i class="fas fa-quote-left d-block text-left"></i></p>
-                        </blockquote>
-                        <cite class="d-block text-left">שם הלקוח</cite>
+                        <div class="testimonial-item testimonials-reveal">
+                            <blockquote id="testimmonial-1" class="blockquote">
+                                <i class="fas fa-quote-right"></i>
+                                <p class="text-center">
+                                    צציטוט ההמלצה של הלקוח והחוויה החיובית והנפלאה שלו עם תום, לנסות להוציא מהלקוחות המלצה של שניים שלושה משפטים<br><i class="fas fa-quote-left d-block text-left"></i></p>
+                            </blockquote>
+                            <cite class="d-block text-left">שם הלקוח</cite>
+                            <hr class="w-50">
+                        </div>
+                        <div class="testimonial-item testimonials-reveal">
+                            <blockquote id="testimmonial-2" class="blockquote">
+                                <i class="fas fa-quote-right"></i>
+                                <p class="text-center">
+                                    יטוט ההמלצה של הלקוח והחוויה החיובית והנפלאה שלו עם תום, לנסות להוציא מהלקוחות המלצה של שניים שלושה משפטים<br><i class="fas fa-quote-left d-block text-left"></i></p>
+                            </blockquote>
+                            <cite class="d-block text-left">שם הלקוח</cite>
+                            <hr class="w-50">
+                        </div>
+                        <div class="testimonial-item testimonials-reveal">
+                            <blockquote id="testimmonial-3" class="blockquote">
+                                <i class="fas fa-quote-right"></i>
+                                <p class="text-center">
+                                    צציטוט ההמלצה של הלקוח והחוויה החיובית והנפלאה שלו עם תום, לנסות להוציא מהלקוחות המלצה של שניים שלושה משפטים<br><i class="fas fa-quote-left d-block text-left"></i></p>
+                            </blockquote>
+                            <cite class="d-block text-left">שם הלקוח</cite>
+                        </div>
                     </section>
                     <!-- testimonials END -->
                     </div>
@@ -221,10 +227,10 @@ include_once "header.php";
         <section id="about" class="mb-3">
             <div class="container-fluid">
                 <div class="row no-gutters">
-                    <div class="col-6 d-none d-lg-block">
+                    <div class="col-6 d-none d-lg-block reveal">
                         <img class="d-block w-100" src="img/trimmer-on-lawn.jpg" alt="">
                     </div>
-                    <div class="col-lg-6 text-center align-self-center mb-3 mb-lg-0 img-bg">
+                    <div class="col-lg-6 text-center align-self-center mb-3 mb-lg-0 img-bg reveal">
                         <h3 class="mt-3 mt-lg-0">אודות</h3>
                         <p class="px-2 px-sm-4 px-md-5">המון טקסט רנדומלי שמספר על תום והעסק שלוהמון טקסט רנדומלי שמספר על תום והעסק שלוהמון טקסט רנדומלי שמספר על תום והעסק שלוהמון טקסט רנדומלי שמספר על תום והעסק שלוהמון טקסט רנדומלי שמספר על תום והעסק שלוהמון טקסט רנדומלי שמספר על תום והעסק שלוהמון טקסט רנדומלי שמספר על תום והעסק שלוהמון טקסט רנדומלי שמספר על תום והעסק שלוהמון טקסט רנדומלי שמספר על תום והעסק שלוהמון טקסט רנדומלי שמספר על תום והעסק שלוהמון טקסט רנדומלי שמספר על תום והעסק שלוהמון טקסט רנדומלי שמספר על תום והעסק שלוהמון טקסט רנדומלי שמספר על תום והעסק שלוהמון טקסט רנדומלי שמספר על תום והעסק שלוהמון טקסט רנדומלי שמספר על תום והעסק שלו</p>
                     </div>
---
+++ js/reveal.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal('.reveal', { delay: 150, duration: 1000, easing: 'ease-in-out' });
+ScrollReveal().reveal('.benefits-reveal', { interval: 250, duration: 1200, easing: 'ease-in-out' });
+ScrollReveal().reveal('.services-reveal', { interval: 200, duration: 1000, easing: 'ease-in-out' });
+ScrollReveal().reveal('.testimonials-reveal', { interval: 250, duration: 1000, easing: 'ease-in-out' });
+ScrollReveal().reveal('.thumb-reveal', { interval: 40, duration: 1000, easing: 'ease-in-out' });
